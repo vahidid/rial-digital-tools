554499a144245afb905f8c3cb5d6914452b9c346
Vahid Hasani <vahidhasani09@gmail.com>
2024-10-31 10:38:26 +0330

    Add web worker and test call

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 export function example(): string {
+  const worker = new Worker('worker.ts');
+  worker.onmessage = ({ data }) => {
+    const { action, payload } = data;
+    switch (action) {
+      case 'log':
+        console.log(`worker.log: ${payload}`);
+        break;
+      case 'result':
+        console.log('RESULT', payload);
+        break;
+      default:
+        console.error(`Unknown action: ${action}`);
+    }
+  };
+
   return 'hello world';
 }
